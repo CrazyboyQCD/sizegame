fea5bb4f8e29a6e85ebd8756a4b2cde522414fa5
CrazyboyQCD <yzc13697716338@gmail.com>
2026-04-15 14:36:36 +0800

    Update Zig hello world to use process.Init parameter for IO access

--- zig/hello.zig
+++ zig/hello.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
-pub fn main() !void {
-    const io = std.Io.Threaded.global_single_threaded.io();
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
 
     try std.Io.File.stdout().writeStreamingAll(io, "Hello, World!\n");
 }
