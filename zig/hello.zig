0c7588557a24e974af7c2494e7bd159a06981e17
CrazyboyQCD <yzc13697716338@gmail.com>
2026-04-15 12:01:04 +0800

    Update Zig hello world to use new std.Io API for Zig 0.15.x

--- zig/hello.zig
+++ zig/hello.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, World!\n", .{});
+    const io = std.Io.Threaded.global_single_threaded.io();
+
+    try std.Io.File.stdout().writeStreamingAll(io, "Hello, World!\n");
 }
